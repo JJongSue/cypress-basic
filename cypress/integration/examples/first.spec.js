c6f485900f935ff25dc9e525d47d0794f203167e
jspark940321 <jspark940321@gmail.com>
2021-05-25 23:24:18 +0900

    feat: +버튼 Max값 12 테스트
    +버튼 클릭시 Max값이 12가 되는지 확인하는 테스트

--- cypress/integration/examples/first.spec.js
+++ cypress/integration/examples/first.spec.js
@@ -11,4 +11,27 @@ describe('ui-counter', () => {
     // value 클래스를 가진 요소의 텍스트가 10
     cy.get('.count-display').should('have.value', '11');
   });
+
+  it('10에서 +버튼 3번 클릭 시 12까지만 증가한다.', () => {
+    // plus-button 클래스를 가진 요소를 클릭
+    cy.get('.plus-button').click();
+    
+    // value 클래스를 가진 요소의 텍스트가 12
+    cy.get('.count-display').should('have.value', '11');
+
+    // plus-button 클래스를 가진 요소를 클릭
+    cy.get('.plus-button').click();
+    
+    // value 클래스를 가진 요소의 텍스트가 12
+    cy.get('.count-display').should('have.value', '12');
+
+    // plus-button 클래스를 가진 요소를 클릭
+    cy.get('.plus-button').click();
+    
+    // value 클래스를 가진 요소의 텍스트가 12
+    cy.get('.count-display').should('have.value', '12');
+  });
+
+
+
 });
